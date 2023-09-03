2eb585f10c9513269a1065b462d5f921c33e6aae
SBodapati11 <shanmukhabodapati@gmail.com>
2023-09-03 15:13:05 -0500

    Additional routes to get all users and get specific user

--- src/routes/user.py
+++ src/routes/user.py
@@ -4,6 +4,7 @@ from flask import request, Blueprint
 
 usersAPI = Blueprint("usersAPI", __name__)
 
+# Create a user
 @usersAPI.route('/users', methods=['POST'])
 def create_user():
     username = request.json['username']
@@ -13,3 +14,18 @@ def create_user():
     with session_scope() as db_session:
         db_session.add(user)
     return format_user(user), 201
+
+# Get all users
+@usersAPI.route('/users/', methods=['GET'])
+def get_users():
+    with session_scope() as db_session:
+        users = db_session.query(User).all()
+        users_list = [format_user(user) for user in users]
+    return users_list, 200
+
+# Get a specific user
+@usersAPI.route('/users/<string:username>', methods=['GET'])
+def get_user(username):
+    with session_scope() as db_session:
+        user = db_session.query(User).filter(User.username == username)[0]
+    return format_user(user), 200
